9ce7e289ba9be91f0b3610d11a06d609cfabcf45
Excoh <excel.ortega@gmail.com>
2024-08-12 21:55:25 -0700

    Add easing animation to camera path follow

--- Scripts/path_controller.gd
+++ Scripts/path_controller.gd
@@ -4,9 +4,18 @@ extends Path2D
 @export var follow_speed: float = 0.15
 
 func _process(delta: float) -> void:
+	var progress_ratio: float = 0
+	var hasChanged: bool = false
 	if Input.is_action_just_pressed("ui_right"):
-		var tween: Tween = create_tween()
-		tween.tween_property($PathFollow2D, "progress_ratio", $PathFollow2D.progress_ratio+progress_interval, follow_speed)
+		progress_ratio = fmod($PathFollow2D.progress_ratio + progress_interval, (1+progress_interval))
+		hasChanged = true
 	elif Input.is_action_just_pressed("ui_left"):
+		if $PathFollow2D.progress_ratio - progress_interval < 0:
+			progress_ratio = 1.0
+		else:
+			progress_ratio = fmod($PathFollow2D.progress_ratio - progress_interval, (1+progress_interval))
+		hasChanged = true
+
+	if hasChanged:
 		var tween: Tween = create_tween()
-		tween.tween_property($PathFollow2D, "progress_ratio", $PathFollow2D.progress_ratio-progress_interval, follow_speed)
+		tween.tween_property($PathFollow2D, "progress_ratio", progress_ratio, follow_speed).set_trans(Tween.TRANS_SPRING).set_ease(Tween.EASE_OUT)
